3986c7e4b2a71f5cf7903cc329cd8feeabcb5782
Pasha <tifahpashaa@gmail.com>
2024-10-10 16:13:04 +0700

    operator logika

---
+++ bin/Tugas1_Pasha/operator_logika.dart
@@ -0,0 +1,10 @@
+
+void main() {
+  bool a = true;
+  bool b = false;
+
+  // Operator logika
+  print('a AND b: ${a && b}'); // Hasil: false
+  print('a OR b: ${a || b}');  // Hasil: true
+  print('NOT a: ${!a}');        // Hasil: false
+}
