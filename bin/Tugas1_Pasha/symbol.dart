440896327f7192fd9ce6221ce907bf6e03834ccf
Pasha <tifahpashaa@gmail.com>
2024-10-10 16:17:12 +0700

    symbol

---
+++ bin/Tugas1_Pasha/symbol.dart
@@ -0,0 +1,10 @@
+
+void main() {
+  // Inisialisasi Symbol
+  Symbol symbol1 = Symbol('Pasha'); // Menggunakan konstruktor Symbol()
+  var symbol2 = #nugas; // Menggunakan literal symbol (#)
+
+  // Mencetak nilai Symbol
+  print('Symbol 1: $symbol1');
+  print('Symbol 2: $symbol2');
+}
