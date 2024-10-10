c7bdbcc76dabd57c2e5d7bed537361ed9822dfcd
Pasha <tifahpashaa@gmail.com>
2024-10-10 16:10:45 +0700

    operator_aritmatika

---
+++ bin/Tugas1_Pasha/operator_aritmatika.dart
@@ -0,0 +1,21 @@
+
+void main(){
+  var result1 = 12 + 15;
+  var result2 = 200 - 40;
+  var result3 = -8;
+  var result4 = 7;
+  var result5 = 4 * 10;
+  var result6 = 50 / 2;
+  var result7 = 20 ~/ 3;
+  var result8 = 12 % 5;
+
+  print('Hasil Penjumlahan: $result1');
+  print('Hasil Pengurangan: $result2');
+  print('Nilai Negatif: $result3');
+  print('Nilai Positif: $result4');
+  print('Hasil Perkalian: $result5');
+  print('Hasil Pembagian: $result6');
+  print('Hasil Pembagian Bulat: $result7');
+  print('Hasil Modulus: $result8');
+
+}
