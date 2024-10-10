816df5cde67279d051e59324cc9da7f27018053b
Pasha <tifahpashaa@gmail.com>
2024-10-10 16:20:32 +0700

    null

---
+++ bin/Tugas1_Pasha/null.dart
@@ -0,0 +1,8 @@
+
+void main() {
+  int? number1;     // number1 sekarang nullable
+  int? number2;     // number2 tetap nullable
+
+  print(number1);   // Output: null
+  print(number2);   // Output: null
+}
