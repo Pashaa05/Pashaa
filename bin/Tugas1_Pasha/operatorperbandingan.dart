8f7c318cfc610fb1ac95b6862e8959bfd6650c0a
Pasha <tifahpashaa@gmail.com>
2024-10-10 16:11:32 +0700

    operator_perbandingan

---
+++ bin/Tugas1_Pasha/operatorperbandingan.dart
@@ -0,0 +1,12 @@
+
+void main() {
+  int a = 15;
+  int b = 10;
+
+  print('a == b: ${a == b}'); // Sama dengan
+  print('a != b: ${a != b}'); // Tidak sama dengan
+  print('a > b: ${a > b}');   // Lebih besar
+  print('a < b: ${a < b}');   // Lebih kecil
+  print('a >= b: ${a >= b}'); // Lebih besar atau sama dengan
+  print('a <= b: ${a <= b}'); // Lebih kecil atau sama dengan
+}
