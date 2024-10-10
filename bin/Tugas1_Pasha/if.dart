e2499d35ffc461f027dd13a2ffba8bbb6d653746
Pasha <tifahpashaa@gmail.com>
2024-10-10 16:17:57 +0700

    if

---
+++ bin/Tugas1_Pasha/if.dart
@@ -0,0 +1,8 @@
+
+void main() {
+  var nilai = 80;
+  var absen = 90;
+
+  // Mengecek apakah nilai dan absen memenuhi syarat kelulusan
+  print((nilai >= 75 && absen >= 75) ? 'Anda Lulus' : 'Anda Tidak Lulus');
+}
