298e41c2af7154c5bcbe99649fbad95f3faf5794
Pasha <tifahpashaa@gmail.com>
2024-10-10 16:15:22 +0700

    list

---
+++ bin/Tugas1_Pasha/list.dart
@@ -0,0 +1,19 @@
+
+void main() {
+  // Create List of int
+  List<int> intList = [];
+
+  // Create List of String
+  List<String> stringList = [];
+
+  // Contoh penambahan elemen
+  intList.add(1);
+  intList.add(2);
+  
+  stringList.add('Haii');
+  stringList.add('Pasha');
+
+  // Menampilkan isi List
+  print('List of int: $intList'); // Output: List of int: [1, 2]
+  print('List of String: $stringList'); // Output: List of String: [Hello, World]
+}
