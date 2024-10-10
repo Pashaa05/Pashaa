7a3359a3ceac834ba5c754a5a7a0d42a1242dec3
Pasha <tifahpashaa@gmail.com>
2024-10-10 16:16:27 +0700

    map

---
+++ bin/Tugas1_Pasha/map.dart
@@ -0,0 +1,12 @@
+
+void main() {
+  // Inisialisasi Map untuk Dart
+  Map<String, String> person = {};
+  Map<String, String> product = {}; // Menggunakan pendekatan yang konsisten untuk inisialisasi Map
+  Map<String, String> address = {};
+
+  // Mencetak isi dari masing-masing Map
+  print("Person Map: $person");
+  print("Product Map: $product");
+  print("Address Map: $address");
+}
