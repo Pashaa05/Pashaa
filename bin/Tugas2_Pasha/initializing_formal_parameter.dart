c5e86e299cdc231ca980911a1c6938503edb39df
Pasha <tifahpashaa@gmail.com>
2024-10-22 14:29:08 +0700

    initializing_formal_parameter

---
+++ bin/Tugas2_Pasha/initializing_formal_parameter.dart
@@ -0,0 +1,14 @@
+void main() {
+  var orang = Orang("Pasha", "Banjarmasin");
+  print("Nama: ${orang.nama}");
+  print("Alamat: ${orang.alamat}");
+  print("Negara: ${orang.negara}");
+}
+
+class Orang {
+  String nama = "Pasha";
+  String? alamat;
+  final String negara = "Indonesia";
+
+  Orang(this.nama, this.alamat);
+}
