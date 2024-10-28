18ba7010bc1344d10b690b7dcc44a45094aa5f83
Pasha <tifahpashaa@gmail.com>
2024-10-28 14:23:49 +0700

    import

---
+++ bin/Tugas2_Pasha/import.dart
@@ -0,0 +1,14 @@
+
+class Category {
+  String id;
+  String name;
+
+  Category(this.id, this.name);
+}
+//import 'data/category.dart';
+void main() {
+  var category = Category("1", "Pasha");
+
+  print(category.id);   
+  print(category.name); 
+}
